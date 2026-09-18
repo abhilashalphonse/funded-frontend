5342c0c59359d6c10abc622783df65734883d08e
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-18 19:57:41 +0100

    Instrument homepage acquisition events

--- src/components/ui/Homepage.jsx
+++ src/components/ui/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from './Hero';
 import TrustSection from './TrustSection';
 import HowItWorksSection from './HowItWorks';
@@ -7,8 +7,24 @@ import Support from './Support';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import BuildChallenge from './BuildChallenge';
+import { captureAttribution, trackEvent } from '../../utils/analytics.js';
 
 function Homepage({ onSignIn, onDashboard, onGetStarted, onSelectPlan, onFreeTrial }) {
+  useEffect(() => {
+    captureAttribution();
+    void trackEvent("landing_view");
+  }, []);
+
+  const startChallenge = (source) => {
+    void trackEvent("hero_cta_click", { source, intent: "paid" }, { entryIntent: "paid" });
+    onGetStarted?.();
+  };
+
+  const startTrial = (source) => {
+    void trackEvent("free_trial_click", { source, intent: "trial" }, { entryIntent: "trial" });
+    onFreeTrial?.();
+  };
+
   const scrollToHowItWorks = () => {
     document.getElementById('how-it-works')?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   };
@@ -19,13 +35,13 @@ function Homepage({ onSignIn, onDashboard, onGetStarted, onSelectPlan, onFreeTri
 
   return (
     <>
-      <Navbar onSignIn={onSignIn} onDashboard={onDashboard} onGetStarted={onGetStarted} />
-      <Hero onGetStarted={onGetStarted} onFreeTrial={onFreeTrial} onSeeHowItWorks={scrollToHowItWorks} />
+      <Navbar onSignIn={onSignIn} onDashboard={onDashboard} onGetStarted={() => startChallenge("navbar")} />
+      <Hero onGetStarted={() => startChallenge("hero")} onFreeTrial={() => startTrial("hero_badge")} onSeeHowItWorks={scrollToHowItWorks} />
       <TrustSection />
       <div id="challenges">
         <BuildChallenge onSelectPlan={onSelectPlan} />
       </div>
-      <HowItWorksSection onGetFunded={onGetStarted} onLearnMore={scrollToChallenges} />
+      <HowItWorksSection onGetFunded={() => startChallenge("how_it_works")} onLearnMore={scrollToChallenges} />
       <Proof />
       <Support />
       <Footer />
